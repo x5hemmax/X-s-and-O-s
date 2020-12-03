7f95fe0173a4b0d0a7a548677669f82feb503cb2
x5hemmax <50978931+x5hemmax@users.noreply.github.com>
2020-12-03 16:22:03 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 
 var boxes;
 var cross;
-
+var end;
 function setup()
 {
     //canvas size
@@ -24,6 +24,7 @@ function setup()
     ]
     //set the initial player to O
     cross = false;
+    end = false;
     
     
 }
@@ -38,7 +39,7 @@ function draw()
     stroke(255);
     
     lines();
-    
+    win();
     
     
 }
@@ -62,26 +63,26 @@ function lines()
 function mousePressed()
 {
     for(var i = 0; i < boxes.length; i++)
-        {
-            if( (mouseX > boxes[i].xpos && mouseX < boxes[i].xpos+100)&&(mouseY > boxes[i].ypos && mouseY < boxes[i].ypos+100) && boxes[i].val == 0)
-                {
-                    
-                    shapes(boxes[i].xpos+50, boxes[i].ypos+50)
-                    if(!cross)
-                        {
-                            boxes[i].val += 5
-                            cross = true;
-                        }
-                    else
-                        {
-                            boxes[i].val += 1
-                            cross = false;
-                        }
-                
-                    
-                    console.log(cross)
-                }
-        }
+    {
+        if(end == false && (mouseX > boxes[i].xpos && mouseX < boxes[i].xpos+100)&&(mouseY > boxes[i].ypos && mouseY < boxes[i].ypos+100) && boxes[i].val == 0)
+            {
+
+                shapes(boxes[i].xpos+50, boxes[i].ypos+50)
+                if(!cross)
+                    {
+                        boxes[i].val += 5
+                        cross = true;
+                    }
+                else
+                    {
+                        boxes[i].val += 1
+                        cross = false;
+                    }
+
+
+                console.log(cross)
+            }
+    }
 }
 
 // a function to draw the O and X
@@ -93,7 +94,7 @@ function shapes(x,y)
             stroke(255);
             strokeWeight(5);
             line(x-20,y-20,x+20,y+20);
-            line(x+20,y-20,x-20,y+20)
+            line(x+20,y-20,x-20,y+20);
         }
     if(!cross)
         {
@@ -103,4 +104,63 @@ function shapes(x,y)
             fill(0);
             ellipse(x,y,40)
         }
+}
+
+// a function to check if someone has one
+function win()
+{
+    //horizontal win checks
+    if((boxes[0].val + boxes[1].val + boxes[2].val) == 3 || (boxes[0].val + boxes[1].val + boxes[2].val) == 15)
+        {
+            console.log('someone win');
+            line(boxes[0].xpos,boxes[0].ypos+50,boxes[2].xpos+100,boxes[2].ypos+50)
+            end = true;
+        }
+    if((boxes[3].val + boxes[4].val + boxes[5].val) == 3 || (boxes[3].val + boxes[4].val + boxes[5].val) == 15)
+        {
+            console.log('someone win');
+            line(boxes[3].xpos,boxes[3].ypos+50,boxes[5].xpos+100,boxes[5].ypos+50)
+            end = true;
+        }
+    if((boxes[6].val + boxes[7].val + boxes[8].val) == 3 || (boxes[6].val + boxes[7].val + boxes[8].val) == 15)
+        {
+            console.log('someone win');
+            line(boxes[6].xpos,boxes[6].ypos+50,boxes[8].xpos+100,boxes[8].ypos+50)
+            end = true;
+        }
+    
+    //vertical win checks
+    if((boxes[0].val + boxes[3].val + boxes[6].val) == 3 || (boxes[0].val + boxes[3].val + boxes[6].val) == 15)
+        {
+            console.log('someone win');
+            line(boxes[0].xpos+50,boxes[0].ypos,boxes[6].xpos+50,boxes[6].ypos+100)
+            end = true;
+        }
+    if((boxes[1].val + boxes[4].val + boxes[7].val) == 3 || (boxes[1].val + boxes[4].val + boxes[7].val) == 15)
+        {
+            console.log('someone win');
+            line(boxes[1].xpos+50,boxes[1].ypos,boxes[7].xpos+50,boxes[7].ypos+100)
+            end = true;
+        }
+    if((boxes[2].val + boxes[5].val + boxes[8].val) == 3 || (boxes[2].val + boxes[5].val + boxes[8].val) == 15)
+        {
+            console.log('someone win');
+            line(boxes[2].xpos+50,boxes[2].ypos,boxes[8].xpos+50,boxes[8].ypos+100)
+            end = true;
+        }
+    //diagonal win check 
+    if((boxes[0].val + boxes[4].val + boxes[8].val) == 3 || (boxes[0].val + boxes[4].val + boxes[8].val) == 15)
+        {
+            console.log('someone win');
+            line(boxes[0].xpos,boxes[0].ypos,boxes[8].xpos+100,boxes[8].ypos+100)
+            end = true;
+        }
+    if((boxes[2].val + boxes[4].val + boxes[6].val) == 3 || (boxes[2].val + boxes[4].val + boxes[6].val) == 15)
+        {
+            console.log('someone win');
+            line(boxes[2].xpos+100,boxes[2].ypos,boxes[6].xpos,boxes[6].ypos+100)
+            end = true;
+        }
+    
+    
 }
